948658174064cf44cdb4877359346b7af6c16a0f
William Krisley <will.ks2101@gmail.com>
2024-12-11 16:38:28 -0300

    fix: rename repository

--- src/api/useCases/master/manager/createManager/index.ts
+++ src/api/useCases/master/manager/createManager/index.ts
@@ -1,18 +1,18 @@
 import { HandlePass } from "../../../../providers/passwords/implementations/HandlePass";
 import { ManagerRepository } from "../../../../repositories/manager/implementation/ManagerRepository";
 import { PermissionRepository } from "../../../../repositories/permissions/implementation/PermissionRepository";
-import { OfficeRepository } from "../../../../repositories/role/implementation/RoleRepository";
+import { RoleRepository } from "../../../../repositories/role/implementation/RoleRepository";
 import { CreateManagerController } from "./createManager.controller";
 import { CreateManagerService } from "./createManager.service";
 
 const handlePass = new HandlePass();
 const managerRepository = new ManagerRepository();
-const officeRepository = new OfficeRepository();
+const roleRepository = new RoleRepository();
 const permissionRepository = new PermissionRepository();
 const createManagerService = new CreateManagerService(
   managerRepository,
   handlePass,
-  officeRepository,
+  roleRepository,
   permissionRepository,
 );
 const createManagerController = new CreateManagerController(
